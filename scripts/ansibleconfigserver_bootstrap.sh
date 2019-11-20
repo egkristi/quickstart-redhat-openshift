3335741017350742c3763a20c698719ccf861e74
Erling Gustav Moland Kristiansen <Erling.Kristiansen@skatteetaten.no>
2019-11-20 13:02:55 +0100

    osm_cluster_network_cidr

--- scripts/ansibleconfigserver_bootstrap.sh
+++ scripts/ansibleconfigserver_bootstrap.sh
@@ -74,6 +74,7 @@ qs_retry_command 10 cfn-init -v --stack ${AWS_STACKNAME} --resource AnsibleConfi
 
 echo openshift_master_cluster_hostname=${INTERNAL_MASTER_ELBDNSNAME} >> /tmp/openshift_inventory_userdata_vars
 echo openshift_master_cluster_public_hostname=${MASTER_ELBDNSNAME} >> /tmp/openshift_inventory_userdata_vars
+echo osm_cluster_network_cidr=${OSM_cluster_SDNSubnetCIDR} >> /tmp/openshift_inventory_userdata_vars
 
 if [ "$(echo ${MASTER_ELBDNSNAME} | grep -c '\.elb\.amazonaws\.com')" == "0" ] ; then
     echo openshift_master_default_subdomain=${MASTER_ELBDNSNAME} >> /tmp/openshift_inventory_userdata_vars
